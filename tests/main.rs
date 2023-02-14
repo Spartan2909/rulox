1bbd18c47c567556dd6769116890a3a4ffbd79b0
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-02-14 12:11:54 +0000

    Add tests

---
+++ tests/main.rs
@@ -0,0 +1,47 @@
+use rulox::lox;
+use rulox_types::prelude::*;
+
+#[test]
+fn var_declaration() {
+    lox! {
+        var value = 5;
+    }
+
+    let _ = value;
+}
+
+#[test]
+fn var_access() {
+    lox! {
+        var value = 5;
+    }
+
+    let _: f64 = value.try_into().unwrap();
+}
+
+#[test]
+fn lox_print() {
+    lox! {
+        print 5;
+    }
+}
+
+#[test]
+fn bools() {
+    lox! {
+        var b = true or false;
+    }
+
+    let value: bool = b.try_into().unwrap();
+
+    assert!(value);
+}
+
+#[test]
+fn arr() {
+    lox! {
+        var a = [5, "hello", false];
+
+        print a;
+    }
+}

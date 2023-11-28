646bf342bde75cda1beb97042917dcf2a66af0c1
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-11-28 16:48:36 +0000

    Fix derive macro

--- rulox_macro/src/lib.rs
+++ rulox_macro/src/lib.rs
@@ -4,11 +4,15 @@
 
 extern crate proc_macro;
 
+use proc_macro2::TokenStream;
 use quote::quote;
 use quote::ToTokens;
+use quote::TokenStreamExt;
 
 use syn::parse_macro_input;
 use syn::DeriveInput;
+use syn::GenericParam;
+use syn::Generics;
 
 mod ast;
 mod ir;
@@ -22,17 +26,42 @@ pub fn lox(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
     program.to_token_stream().into()
 }
 
+fn generic_params(generics: &Generics) -> TokenStream {
+    let mut tokens = TokenStream::new();
+    for generic in &generics.params {
+        if let GenericParam::Lifetime(l) = generic {
+            let lifetime = &l.lifetime;
+            tokens.append_all(quote!(#lifetime,));
+        }
+    }
+    for generic in &generics.params {
+        match generic {
+            GenericParam::Const(c) => {
+                let ident = &c.ident;
+                tokens.append_all(quote!(#ident,));
+            }
+            GenericParam::Type(ty) => {
+                let ident = &ty.ident;
+                tokens.append_all(quote!(#ident,));
+            }
+            GenericParam::Lifetime(_) => unreachable!(),
+        }
+    }
+    tokens
+}
+
 #[proc_macro_derive(TryFromLoxValue)]
 pub fn derive_try_from(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
 
     let name = input.ident;
     let generics = input.generics;
+    let generic_params = generic_params(&generics);
 
     quote! {
-        impl #generics ::core::convert::TryFrom<__rulox_helpers::LoxValue> for #name #generics
+        impl #generics ::core::convert::TryFrom<__rulox_helpers::LoxValue> for #name <#generic_params>
         where
-            #name #generics: ::core::clone::Clone + __rulox_helpers::LoxObject
+            #name <#generic_params>: ::core::clone::Clone + __rulox_helpers::LoxObject
         {
             type Error = __rulox_helpers::LoxError;
 
